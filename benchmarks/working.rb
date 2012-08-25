c14e7dd786da7da6e95fd5e1df5029425c651dad
Jens Bissinger <mail@jens-bissinger.de>
2012-08-25 17:30:40 +0200

    refactored working benchmark, buggy

--- benchmarks/working.rb
+++ benchmarks/working.rb
@@ -3,7 +3,7 @@
 require File.join(File.dirname(__FILE__), '..', 'lib', 'eq', 'boot', 'all')
 
 require 'benchmark'
-require 'tempfile'
+require 'tmpdir'
 
 EQ.logger.level = Logger::Severity::ERROR
 
@@ -12,24 +12,39 @@ class MyJob
   end
 end
 
-n = 500
-Benchmark.bm(50) do |b|
-  EQ.boot_queueing
-  n.times { |i| EQ.queue.push! MyJob, i }
-  b.report('memory-based sqlite') do
-    EQ.boot_working
-    sleep 0.01 until EQ.queue.waiting.count == 0
+class BenchmarkHelper < Struct.new(:n, :benchmark)
+  def report name, &configure
+    EQ.config &configure
+    EQ.boot
+    n.times { |i| EQ.queue.push! MyJob, i }
+    benchmark.report name do
+      EQ.boot_working
+      sleep 0.01 until EQ.queue.waiting.count == 0
+    end  
+    EQ.shutdown
+    sleep 0.05
   end
-  EQ.shutdown
+end
 
-  EQ.config do |config|
-    config[:sqlite] = Tempfile.new('').path
+n = 100
+Benchmark.bm(50) do |b|
+  helper = BenchmarkHelper.new(n, b)
+
+  helper.report 'memory-based sqlite' do |config|
+    config.queue = 'sequel'
   end
-  EQ.boot_queueing
-  n.times { |i| EQ.queue.push! MyJob, i }
-  b.report('file-based sqlite') do
-    EQ.boot_working
-    sleep 0.01 until EQ.queue.waiting.count == 0
+
+  helper.report 'file-based-sqlite' do |config|
+    config.queue = 'sequel'
+    config.sequel = "sqlite://#{Dir.mktmpdir}/benchmark.sqlite3"
   end
-  EQ.shutdown
+
+  helper.report 'sorted set' do |config|
+    config.queue = 'sorted_set'
+  end
+
+  helper.report 'leveldb' do |config|
+    config.queue = 'leveldb'
+    config.leveldb = Dir.mktmpdir
+  end
 end
