072be6d0a4c59c9a64152d05da408614475afaed
Jens Bissinger <mail@jens-bissinger.de>
2012-08-26 11:30:28 +0200

    added pending specs for worker class

---
+++ spec/lib/eq-working/worker_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe EQ::Working::Worker do
+  subject { EQ::Working::Worker.new false }
+
+  it 'loops for new jobs' do
+    pending
+  end
+
+  it 'performs jobs' do
+    pending
+  end
+end
