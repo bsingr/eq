23b957ab0dc4f90a3fb77c58911f9197444e56d0
Jens Bissinger <mail@jens-bissinger.de>
2012-08-27 19:41:18 +0200

    added spec to prove behavior of in-memory queues

---
+++ spec/lib/eq-queueing_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe EQ do
+  context 'transient queueing' do
+    it "won't survives at least a dying queue actor" do
+      # setup a in-memory queue backend
+      EQ.config {|c| c.queue = 'sequel'; c.sequel = 'sqlite:/'}
+      EQ.boot
+      EQ.queue.push "foo"
+      EQ.queue.count.should == 1
+      EQ.shutdown
+      EQ.boot
+      EQ.queue.count.should == 0
+    end
+  end
+end
